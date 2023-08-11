235efe75610ae05a8b78671b1397c3a72dcc6b7a
Matt Jennings <matt@mattjennings.net>
2023-08-10 20:16:49 -0700

    done with fixing eslint issues

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const commonConfig = require('./webpack.common.js'); // eslint-disable-line impo
 
 module.exports = (envVars) => {
   const { env } = envVars;
-  const envConfig = require(`./webpack.${env}.js`); // eslint-disable-line global-require, import/no-dynamic-require, import/extensions
+  const envConfig = require(`./webpack.${env}.js`); // eslint-disable-line import/no-dynamic-require, global-require
   const config = merge(commonConfig, envConfig);
   return config;
 };
